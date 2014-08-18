db31668b3a1265621078000df1d7656ca89561cf
fland <gaus.fland@gmail.com>
2014-08-18 17:40:17 +0300

    + commit/push test

--- src/main/java/ua/pp/fland/web/bookkeeping/Main.java
+++ src/main/java/ua/pp/fland/web/bookkeeping/Main.java
@@ -17,6 +17,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        LOG.debug("debug: Hello world!");
+        LOG.debug("Hello world!");
     }
 }
